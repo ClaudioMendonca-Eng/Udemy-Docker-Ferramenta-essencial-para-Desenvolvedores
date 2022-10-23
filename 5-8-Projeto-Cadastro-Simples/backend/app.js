e508cb6a6594ee6faf19675dbc76f9037553e57b
Claudio Mendonça <contato@claudiomendonca.eng.br>
2022-10-23 10:19:32 -0300

    3. Finalizando o Cadastro

--- 5-8-Projeto-Cadastro-Simples/backend/app.js
+++ 5-8-Projeto-Cadastro-Simples/backend/app.js
@@ -2,13 +2,38 @@ const express = require('express')
 const restful = require('node-restful')
 const server = express()
 const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
+//************************
 // Teste 
-server.get('/', (req, res, next) => res.send('Backend!'));
-
+//server.get('/', (req, res, next) => res.send('Backend!'));
 //Start server
-server.listen(3000);
+//server.listen(3000);
+//************************
+
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+
+
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
+
+// Start Server
+server.listen(3000)
